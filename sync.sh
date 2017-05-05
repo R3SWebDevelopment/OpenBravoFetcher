a5c84a9464cddf79722e560eec13638aafc81ac0
Ricardo Tercero Solis <ricardo.tercero.solis@gmail.com>
2017-05-05 10:38:12 -0500

    Update bash script

--- sync.sh
+++ sync.sh
@@ -4,6 +4,7 @@
 : ${REMOTE_KEY:=${ROOT}"/baysingers_production_20160627.pem"}
 : ${SYNC_FOLDER:=${ROOT}"/sync/"}
 : ${SQL_FOLDER:=${ROOT}"/syncPushSQL/"}
+: ${CSV_FOLDER:=${ROOT}"/csv/"}
 : ${REMOTE_ROOT_FOLDER:="/home/ubuntu/.virtualenvs/baysingers-v3/"}
 : ${REMOTE_CURRENT_FOLDER:=${REMOTE_ROOT_FOLDER}"current/"}
 : ${REMOTE_JSON_FOLDER:=${REMOTE_CURRENT_FOLDER}"sync/"}
@@ -13,9 +14,12 @@ cd ${SYNC_FOLDER}
 rm *.json
 cd ${SQL_FOLDER}
 rm *.sql
+cd ${CSV_FOLDER}
+rm *.csv
 cd ${ROOT}
 python < obAPI.py
 ssh -i ${REMOTE_KEY} ubuntu@baysingers.com "cd ${REMOTE_ROOT_FOLDER} ; ./cleanSync.sh"
 rsync -e "ssh -i ${REMOTE_KEY}" --verbose --progress --recursive --archive ${SYNC_FOLDER} ubuntu@baysingers.com:${REMOTE_JSON_FOLDER}
 rsync -e "ssh -i ${REMOTE_KEY}" --verbose --progress --recursive --archive ${SQL_FOLDER} ubuntu@baysingers.com:${REMOTE_SQL_FOLDER}
+rsync -e "ssh -i ${REMOTE_KEY}" --verbose --progress --recursive --archive ${CSV_FOLDER} ubuntu@baysingers.com:${CSV_FOLDER}
 #ssh -i ${REMOTE_KEY} ubuntu@baysingers.com "cd ~ ; ./runSync.sh"

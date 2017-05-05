7ce27c7b3a0edda12a087edb5af191ba6ead3497
Ricardo Tercero Solis <ricardo.tercero.solis@gmail.com>
2017-05-05 10:43:55 -0500

    Update bash script

--- sync.sh
+++ sync.sh
@@ -9,6 +9,7 @@
 : ${REMOTE_CURRENT_FOLDER:=${REMOTE_ROOT_FOLDER}"current/"}
 : ${REMOTE_JSON_FOLDER:=${REMOTE_CURRENT_FOLDER}"sync/"}
 : ${REMOTE_SQL_FOLDER:=${REMOTE_CURRENT_FOLDER}"syncPushSQL/"}
+: ${REMOTE_CSV_FOLDER:=${REMOTE_CURRENT_FOLDER}"csv/"}
 
 cd ${SYNC_FOLDER}
 rm *.json
@@ -21,5 +22,5 @@ python < obAPI.py
 ssh -i ${REMOTE_KEY} ubuntu@baysingers.com "cd ${REMOTE_ROOT_FOLDER} ; ./cleanSync.sh"
 rsync -e "ssh -i ${REMOTE_KEY}" --verbose --progress --recursive --archive ${SYNC_FOLDER} ubuntu@baysingers.com:${REMOTE_JSON_FOLDER}
 rsync -e "ssh -i ${REMOTE_KEY}" --verbose --progress --recursive --archive ${SQL_FOLDER} ubuntu@baysingers.com:${REMOTE_SQL_FOLDER}
-rsync -e "ssh -i ${REMOTE_KEY}" --verbose --progress --recursive --archive ${CSV_FOLDER} ubuntu@baysingers.com:${CSV_FOLDER}
+rsync -e "ssh -i ${REMOTE_KEY}" --verbose --progress --recursive --archive ${CSV_FOLDER} ubuntu@baysingers.com:${REMOTE_CSV_FOLDER}
 #ssh -i ${REMOTE_KEY} ubuntu@baysingers.com "cd ~ ; ./runSync.sh"
